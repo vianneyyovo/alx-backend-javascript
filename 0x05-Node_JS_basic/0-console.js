933fcd8baf20638f37d4c60b1d245dd8979d2455
vianneyyovo <vianneyyovo@gmail.com>
2023-03-21 13:34:23 +0000

    Create 0x05-Node_JS_basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Executing basic javascript with Node JS
+ * 
+ * Displays a message on the STDOUT.
+ * @param {String} message The message to display.
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+/**
+ * Using Process stdin
+ */
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
